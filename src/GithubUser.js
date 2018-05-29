669e81c831664a30dff198e86903880204f203cb
UnnatiPulla <upulla@purdue.edu>
2018-05-29 18:43:07 -0400

    added rover info and other components

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,26 +1,44 @@
-import React, {Component} from "react"
+import React, { Component } from 'react'
 
-class GithubUser extends Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            user : {}
-        }
+import './GithubUser.css'
 
-        this.fetchUserData();
+class GithubUser extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: {}
     }
 
-    fetchUserData = () => {
-        fetch('https://api.github.com/users/${this.props.match.params.username}')
-        .then(response => response.json())
-        .then(user => this.setState({user}))
+    this.fetchUserData(props)
+  }
+
+  componentWillReceiveProps = (newProps) => {
+    const locationChanged = newProps.location !== this.props.location
+    if (locationChanged) {
+      this.fetchUserData(newProps)
     }
-    render(){
-        return(
-            <div classname="GithubUser">
-                <h1>Github User: {this.props.match.params.username}</h1>
-            </div>
-        )
-    }
+  }
+
+  fetchUserData = (props) => {
+    fetch(`https://api.github.com/users/${props.match.params.username}`)
+      .then(response => response.json())
+      .then(user => this.setState({ user }))
+  }
+
+  render() {
+    const { user } = this.state
+    return (
+      <div className="GithubUser">
+        <img src={user.avatar_url} alt=""/>
+        <h2>{user.login}</h2>
+        <h3>followers: {user.followers}</h3>
+        <h3>following: {user.following}</h3>
+        <h3>location: {user.location}</h3>
+        <a href={user.html_url} target="_blank">Link to {user.login}'s profile</a>
+      </div>
+    )
+  }
 }
+
 export default GithubUser
