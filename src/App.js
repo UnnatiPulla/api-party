b2cba4ea90a7a9ec0759b8849931c7a25875ab45
UnnatiPulla <upulla@purdue.edu>
2018-05-29 15:00:07 -0400

    Style header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="App-header">
+          <h3>Ain't no party like API party</h3>
         <h1>API Party</h1>
+        </div>
       </div>
     );
   }
