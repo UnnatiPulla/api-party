a3fb8d8a9f4c554ffc69fcbe35e343e9505545e8
UnnatiPulla <upulla@purdue.edu>
2018-05-29 15:31:46 -0400

    Make route to match github username

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Route, Switch, NavLink } from 'react-router-dom'
 
-import { Route, Switch, NavLink} from "react-router-dom"
-
-import './App.css';
+import './App.css'
+import Github from './Github'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
         <div className="App-header">
-          <h3>Ain't no party like API party</h3>
-        <h1>API Party</h1>
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
         </div>
-        <ul classname ="navLinks">
-          <li><NavLink to="/github">Github API</NavLink></li>
-          </ul>
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+        </ul>
         <Switch>
-          <Route path="/github" render={() => <h1>Github!</h1>}/>
-          <Route render={() => <p>To get started, click one of the links above</p>}/>
+          <Route path="/github" component={Github} />
+          <Route render={() => <p>To get started, click one of the links above.</p>} />
         </Switch>
-
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
---
+++ src/Github.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+import './Github.css'
+
+class Github extends Component {
+  state = {
+    username: '',
+  }
+
+  handleChange = (ev) => {
+    this.setState({ username: ev.target.value })
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.props.history.push(`/github/${this.state.username}`)
+  }
+
+  render() {
+    return (
+      <div className="Github">
+        <img
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+          alt="GitHub"
+          className="logo"
+        />
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Look up GitHub user</button>
+          </div>
+        </form>
+        <Route path="/github/:username" render={(props) => <h3>You entered {props.match.params.username}!</h3>} />
+        <Route exact path="/github" render={() => <h3>Please enter a username to search on GitHub.</h3>} />
+      </div>
+    )
+  }
+}
+
+export default Github
